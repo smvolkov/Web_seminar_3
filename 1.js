3d289010cbf89d3b9758e5093a4c8a25652a07e6
Semion Volkov <semen_volkov@mail.ru>
2023-01-23 11:38:48 +0500

    Task1 updated

--- 1.js
+++ 1.js
@@ -1,3 +1,3 @@
-temperatureC = +prompt('Введите температуру в градусах цельсия')
-temperatureF = (9/5) * temperatureC + 32
+let temperatureC = +prompt('Введите температуру в градусах цельсия')
+let temperatureF = (9/5) * temperatureC + 32
 alert(`Цельсий: ${temperatureC}, Фаренгейт: ${temperatureF.toFixed(2)}`)
